ba8bcbda0c1bac277d100622250ea64c1378a1d1
IK Mendoza <iken.mndz@gmail.com>
2025-11-28 13:42:40 +0800

    app setup

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,6 +15,9 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
+    protected $model = \App\Models\User::class;
+    
     public function definition()
     {
         return [
@@ -22,7 +25,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'role' => $this->faker->randomElement(['admin', 'customer']),
         ];
     }
 
