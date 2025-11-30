632639eefbaf1f7e84428e2ab96d544a93b026f5
IK Mendoza <iken.mndz@gmail.com>
2025-11-30 21:50:29 +0800

    Factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'role' => $this->faker->randomElement(['admin', 'customer']),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi' // password
         ];
     }
 
