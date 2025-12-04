545995def5c3429d8ff45f3296e6e6ae98f5bb1f
Renz Diaz <diazrenz30@gmail.com>
2025-12-04 13:35:24 +0800

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
+            CategorySeeder::class,
+            ProductsSeeder::class,
+            RolesSeeder::class,
             UsersSeeder::class
         ]);
     }
